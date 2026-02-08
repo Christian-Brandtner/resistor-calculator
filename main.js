d6d96bf13c2e69da405fecc763b946ae3ebf877f
Christian-Brandtner <christianbrandtner32@gmail.com>
2026-02-07 17:11:33 -0800

    add basic state switching

--- main.js
+++ main.js
@@ -8,17 +8,26 @@ function calculate() {
     r2 = parseFloat(document.getElementById("resistor-2").value);
     voltage = parseFloat(document.getElementById("voltage").value);
     unit = document.getElementById("units").value;
+    const outputVoltage = ((r2 / (r1 + r2)) * voltage);
 
-    if (r1 > 0 && r2 > 0) {
-        const outputVoltage = ((r2 / (r1 + r2)) * voltage);
+    if (!isNaN(outputVoltage) && (r1 != 0 && r2 != 0)) {
         document.getElementById("output-voltage").value = outputVoltage.toFixed(2) + " V";
+    } else if (r1 == 0 && r2 == 0) {
+        if (!isNaN(voltage)) {
+
+            document.getElementById("output-voltage").value = voltage.toFixed(2) + " V";
+        } else {
+            document.getElementById("output-voltage").value = "Error";
+        }
     } else {
-        document.getElementById("output-voltage").value = voltage.toFixed(2) + " V";
+        document.getElementById("output-voltage").value = "Error"
     }
+
+
 }
 
 function toggleMode() {
-    
+
     body = document.getElementsByTagName("body")[0];
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
@@ -31,4 +40,25 @@ function toggleMode() {
     else {
         body.classList.add("dark");
     }
+
+}
+
+// function changeCalculator() {
+//     // section = document.getElementsByTagName('section')[0];
+//     var div = document.getElementsByClassName("current")[0];
+//     if (div.classList.contains("current")) {
+//         div.remove("current");
+//     }
+
+// }
+
+function changeCalculator(calculator) {
+    var previous = document.getElementsByClassName("current")[0];
+    previous.style.display = "none";
+    previous.classList.remove("current");
+    console.log(previous.classList);
+    var current = document.getElementsByClassName(calculator)[0];
+    current.style.display = "flex";
+    current.classList.add("current");
+    console.log(current.classList);
 }
